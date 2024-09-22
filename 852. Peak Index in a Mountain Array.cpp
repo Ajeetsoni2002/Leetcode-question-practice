21d5b83bf98e7afefac4d6fe2de920bad7b87e3d
Ajeet Kumar Soni <94032343+Ajeetsoni2002@users.noreply.github.com>
2024-09-22 16:34:57 +0530

    leetcode problem

---
+++ 852. Peak Index in a Mountain Array.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+
+int findpeakindex(vector<int>arr){
+    int start=0;
+    int end=arr.size()-1;
+    int mid= start+(end-start)/2;
+
+    while(start<end){
+        if(arr[mid]<arr[mid+1]){
+             start=mid+1;
+        }
+    else{
+        end=mid;
+    }
+    mid=start+(end-start)/2;
+}
+return start;
+}
+    int peakIndexInMountainArray(vector<int>& arr) {
+      return findpeakindex(arr);  
+    }
+};
