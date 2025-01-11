537d88dddf2c7e0dd4394c2ae64ae7055aae1d8c
Ajeet Kumar Soni <94032343+Ajeetsoni2002@users.noreply.github.com>
2025-01-11 09:05:41 +0530

    leetcode problem

--- 1400. Construct K Palindrome Strings.cpp
+++ 1400. Construct K Palindrome Strings.cpp
@@ -1,17 +1,20 @@
 class Solution {
- public:
-  bool canConstruct(string s, int k) {
-    // If |s| < k, we cannot construct k strings from the s.
-    if (s.length() < k)
-      return false;
-
-    bitset<26> odd;
-
-    for (const char c : s)
-      odd.flip(c - 'a');
-
-    // If the number of letters that have odd counts > k, the minimum number of
-    // palindromic strings we can construct is > k.
-    return odd.contains() <= k;
-  }
+public:
+    bool canConstruct(string s, int k) {
+        if(s.size() < k) return false;
+        if(s.size() == k) return true;
+        map<char, int> count;
+        
+        for(char c : s){
+            count[c]++;
+        }
+        
+        int odd = 0;
+        for(auto it = count.begin(); it != count.end(); it++){
+            if(it->second % 2 == 1){
+                odd++;
+            }
+        }
+        return odd <= k;
+    }
 };
