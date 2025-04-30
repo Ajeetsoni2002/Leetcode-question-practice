006fce9236e9f02b02620bbd205468f9bc1599de
Ajeet Kumar Soni <94032343+Ajeetsoni2002@users.noreply.github.com>
2025-04-30 12:02:22 +0530

    leetcode problme

---
+++ 1295. Find Numbers with Even Number of Digits.cpp
@@ -0,0 +1,18 @@
+class Solution {
+    public:
+        int findNumbers(vector<int>& nums) {
+            int ans=0;
+            for(int i=0; i<nums.size();i++){
+                int count=0;
+                
+                while(nums[i]){
+                    count++;
+                    nums[i]=nums[i]/10;
+                }
+                if(count%2==0){
+                    ans++;
+                }
+            }
+            return ans;
+        }
+    };
